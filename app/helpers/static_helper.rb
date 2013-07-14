3579ce3f88ed30a1bd07d9fe7347fbecba5e9fd3
Chris Schmidt <chris@superbilk.org>
2013-07-14 23:20:34 +0200

    resize xing profile images

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,2 +1,23 @@
 module StaticHelper
+
+  def xing_profile_image_url(img_url, size = :mini_thumb)
+    plain_img_url = img_url.match(/(.*\.).+.jpg$/)[1]
+
+    case size
+    when :large
+      img_url = plain_img_url + "140x185.jpg"
+    when :mini_thumb
+      img_url = plain_img_url + "18x24.jpg"
+    when :thumb
+      img_url = plain_img_url + "30x40.jpg"
+    when :medium_thumb
+      img_url = plain_img_url + "57x75.jpg"
+    when :maxi_thumb
+      img_url = plain_img_url + "70x93.jpg"
+    else
+      img_url
+    end
+    img_url
+  end
+
 end
